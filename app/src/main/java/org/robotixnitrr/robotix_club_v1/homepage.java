0f1bd01e635a63a281fe0829b70ef2565b8ea891
dvkcool <divyanshukumarg@gmail.com>
2017-08-18 17:20:27 +0530

    Logout Button added

--- app/src/main/java/org/robotixnitrr/robotix_club_v1/homepage.java
+++ app/src/main/java/org/robotixnitrr/robotix_club_v1/homepage.java
@@ -1,7 +1,11 @@
 package org.robotixnitrr.robotix_club_v1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.facebook.accountkit.AccountKit;
 
 public class homepage extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+    }
+    public void onLogout(View view) {
+        // logout of Account Kit
+        AccountKit.logOut();
+        launchlogin();
+    }
+    public void launchlogin(){
+        startActivity(new Intent(this, LoginScreen.class));
     }
 }
