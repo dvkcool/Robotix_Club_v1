c865882ce76023d331a724b24df5d1ce3e9ccd44
dvkcool <divyanshukumarg@gmail.com>
2017-09-06 21:55:21 +0530

    Adding users to server

--- app/src/main/java/org/robotixnitrr/robotix_club_v1/AccountActivity.java
+++ app/src/main/java/org/robotixnitrr/robotix_club_v1/AccountActivity.java
@@ -1,6 +1,8 @@
 package org.robotixnitrr.robotix_club_v1;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,39 +18,57 @@ import com.google.i18n.phonenumbers.NumberParseException;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.google.i18n.phonenumbers.Phonenumber;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Locale;
 
 public class AccountActivity extends AppCompatActivity {
 
-    TextView id;
+
     TextView infoLabel;
     TextView info;
+    String s=""; int c=0, t=0;
+    String n="";
+    TextView name;
+    ProgressDialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
-        id = (TextView) findViewById(R.id.id);
+        name = (TextView) findViewById(R.id.name);
         infoLabel = (TextView) findViewById(R.id.info_label);
         info = (TextView) findViewById(R.id.info);
+        dialog = ProgressDialog.show(this, "Loading", "Please wait...", true);
 
         AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
             @Override
             public void onSuccess(final Account account) {
 
                 String accountKitId = account.getId();
-                id.setText(accountKitId);
+
 
                 PhoneNumber phoneNumber = account.getPhoneNumber();
                 if (account.getPhoneNumber() != null) {
                     String formattedPhoneNumber = formatPhoneNumber(phoneNumber.toString());
+                    s= formattedPhoneNumber;
                     info.setText(formattedPhoneNumber);
                     infoLabel.setText(R.string.phone_label);
+                    con cq = new con();
+                    cq.execute();
+
                 }
                 else {
                     String emailString = account.getEmail();
+                    s= emailString;
                     info.setText(emailString);
                     infoLabel.setText(R.string.email_label);
+                    con cq = new con();
+                    cq.execute();
+                    t++;
                 }
 
             }
@@ -79,10 +99,82 @@ public class AccountActivity extends AppCompatActivity {
             PhoneNumberUtil pnu = PhoneNumberUtil.getInstance();
             Phonenumber.PhoneNumber pn = pnu.parse(phoneNumber, Locale.getDefault().getCountry());
             phoneNumber = pnu.format(pn, PhoneNumberUtil.PhoneNumberFormat.NATIONAL);
+            String mob="";
+            int l = phoneNumber.length();
+            for(int i=0; i<l; i++){
+                char c = phoneNumber.charAt(i);
+                if(c!=' '){
+                    mob= mob+c;
+                }
+            }
+            phoneNumber=mob;
         }
         catch (NumberParseException e) {
             e.printStackTrace();
         }
         return phoneNumber;
     }
+
+
+
+    class con extends AsyncTask<Void, Void, Boolean> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+             dialog.show();
+
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            try
+            {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection cn = DriverManager.getConnection("jdbc:mysql://sg2plcpnl0175.prod.sin2.secureserver.net:3306/postnitrr", "robotix2017","Robotixrocks@2017");
+                Statement st= cn.createStatement();
+                ResultSet r= st.executeQuery("select userid, name from user;");
+                while(r.next()){
+                    String s1= r.getString(1);
+                    if(s.equals(s1)){
+                        c++;
+                        n = r.getString(2);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                String toastMessage = e.getMessage();
+                Toast.makeText(AccountActivity.this, toastMessage, Toast.LENGTH_LONG).show();
+
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+           dialog.dismiss();
+            if(c==0){
+                registeru();
+            }
+            else{
+                name.setText("Welcome "+n);
+            }
+        }
+    }
+    public void registeru(){
+        Intent intent = new Intent(this, Register.class);
+        intent.putExtra("userid", s);
+        if(t==0){
+            intent.putExtra("mobile1", true);
+            intent.putExtra("mobile", s);
+        }
+        else{
+            intent.putExtra("email1", true);
+            intent.putExtra("email", s);
+        }
+        startActivity(intent);
+
+    }
+
 }
---
+++ app/src/main/java/org/robotixnitrr/robotix_club_v1/Register.java
@@ -0,0 +1,135 @@
+package org.robotixnitrr.robotix_club_v1;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Region;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Register extends AppCompatActivity {
+String mob="", email="", userid;
+    String q="";
+    EditText name, college, branch, sem, mobi, em;
+    ProgressDialog dialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        name = (EditText) findViewById(R.id.nm);
+        college = (EditText) findViewById(R.id.clg);
+        branch = (EditText) findViewById(R.id.branch);
+        sem = (EditText) findViewById(R.id.sem);
+        mobi = (EditText) findViewById(R.id.mobile);
+        em = (EditText) findViewById(R.id.email);
+
+        Intent intent= getIntent();
+        if(intent.getBooleanExtra("mobile1", false)){
+            mob = intent.getStringExtra("mobile");
+            mobi.setText(mob);
+        }
+        if(intent.getBooleanExtra("email1", false)){
+            email= intent.getStringExtra("email");
+            em.setText(email);
+        }
+ userid = intent.getStringExtra("userid");
+
+
+    }
+    public void submitclc(View view) {
+        do {
+            String n = name.getText().toString();
+
+            String c = college.getText().toString();
+            String b = branch.getText().toString();
+            String se = sem.getText().toString();
+            String m = mobi.getText().toString();
+            String e = em.getText().toString();
+            if (n.length() == 0) {
+                String toastMessage = "Please enter your name";
+                Toast.makeText(this, toastMessage, Toast.LENGTH_LONG).show();
+                break;
+            }
+            if (n.length() == 0) {
+                String toastMessage = "Please enter your name";
+                Toast.makeText(this, toastMessage, Toast.LENGTH_LONG).show();
+                break;
+            }
+            if (c.length() == 0) {
+                String toastMessage = "Please enter your college";
+                Toast.makeText(this, toastMessage, Toast.LENGTH_LONG).show();
+                break;
+            }
+            if (b.length() == 0) {
+                String toastMessage = "Please enter your branch";
+                Toast.makeText(this, toastMessage, Toast.LENGTH_LONG).show();
+                break;
+            }
+            if (se.length() == 0) {
+                String toastMessage = "Please enter your semester";
+                Toast.makeText(this, toastMessage, Toast.LENGTH_LONG).show();
+                break;
+            }
+            if (m.length() == 0) {
+                String toastMessage = "Please enter your mobile number";
+                Toast.makeText(this, toastMessage, Toast.LENGTH_LONG).show();
+                break;
+            }
+            if (e.length() == 0) {
+                String toastMessage = "Please enter your email id";
+                Toast.makeText(this, toastMessage, Toast.LENGTH_LONG).show();
+                break;
+            }
+            q = "Insert into user values('" + userid + "', '" + n + "', '" + c + "', '" + b + "', '" + se + "', '" + m + "', '" + e + "');";
+            con cq = new con();
+            cq.execute();
+        }while(false);
+    }
+    class con extends AsyncTask<Void, Void, Boolean> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = ProgressDialog.show(Register.this, "Loading", "Please wait...", true);
+            dialog.show();
+
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            try
+            {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection cn = DriverManager.getConnection("jdbc:mysql://sg2plcpnl0175.prod.sin2.secureserver.net:3306/postnitrr", "robotix2017","Robotixrocks@2017");
+                Statement st= cn.createStatement();
+                st.executeUpdate(q);
+
+            }
+            catch (Exception e)
+            {
+                String toastMessage = e.getMessage();
+                Toast.makeText(Register.this, toastMessage, Toast.LENGTH_LONG).show();
+
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            dialog.dismiss();
+            Toast.makeText(Register.this, "Succesfully registered", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(Register.this, AccountActivity.class);
+         startActivity(intent);
+
+        }
+    }
+
+}
